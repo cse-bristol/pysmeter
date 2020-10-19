35820fc4dab5ebe22af5d165865ccbd793306a75
mark <mg8404@my.bristol.ac.uk>
2020-10-19 16:15:48 +0100

    Get model download to work

--- pysmeter/download_model.py
+++ pysmeter/download_model.py
@@ -1,5 +1,4 @@
 import os
-import shutil
 import requests
 from pysmeter.common import MODEL_VERSION, ENSEMBLE_SIZE, MODELS_PATH
 
@@ -8,20 +7,42 @@ _MODEL_URL = "https://smeter.cse.org.uk/models/" + MODEL_VERSION
 
 def download_model():
     """Downloads the model files from the remote server and saves them in the specified dir."""
-    for i in range(ENSEMBLE_SIZE):
-        model_filename = f"model{i}.json"
-        weights_filename = f"model{i}.h5"
+    # Make the models dirs if necessary
+    three_wk_dir = os.path.join(MODELS_PATH, MODEL_VERSION, "3wk")
+    four_wk_dir = os.path.join(MODELS_PATH, MODEL_VERSION, "4wk")
 
-        model_url = f"{_MODEL_URL}/{model_filename}"
-        weights_url = f"{_MODEL_URL}/{weights_filename}"
+    if not os.path.isdir(three_wk_dir):
+        os.makedirs(three_wk_dir)
 
-        with requests.get(model_url, stream=True) as r:
-            with open(os.path.join(MODELS_PATH, model_filename), "wb+") as f:
-                shutil.copyfileobj(r.raw, f)
+    if not os.path.isdir(four_wk_dir):
+        os.makedirs(four_wk_dir)
 
-        with requests.get(weights_url, stream=True) as r:
-            with open(os.path.join(MODELS_PATH, weights_filename), "wb+") as f:
-                shutil.copyfileobj(r.raw, f)
+    for week in ("3wk", "4wk"):
+        # Make the model dir if necessary
+        model_dir = os.path.join(MODELS_PATH, MODEL_VERSION, week)
+
+        if not os.path.isdir(model_dir):
+            os.makedirs(model_dir)
+
+        print(f"Downloading model files for {week} model...")
+
+        # Download the files
+        for i in range(ENSEMBLE_SIZE):
+            model_filename = f"model{i}.json"
+            weights_filename = f"model{i}.h5"
+
+            model_url = os.path.join(_MODEL_URL, week, model_filename)
+            weights_url = os.path.join(_MODEL_URL, week, weights_filename)
+
+            with requests.get(model_url) as r:
+                with open(os.path.join(model_dir, model_filename), "w+") as f:
+                    f.write(r.text)
+
+            with requests.get(weights_url) as r:
+                with open(os.path.join(model_dir, weights_filename), "wb+") as f:
+                    f.write(r.content)
+
+            print(f"Downloaded {i + 1}/{ENSEMBLE_SIZE} model files.")
 
     return MODELS_PATH
 
